7ba06378c7e590487e1a1d32ecca4df1bfa715c0
Irad <Irad@irad-laptop>
2016-02-21 22:53:42 -0800

    hello world one and two

---
+++ src/helloworldone/helloclassone.java
@@ -0,0 +1,7 @@
+package helloworldone;
+
+public class helloclassone {
+	public static void main(String args[]) {
+		System.out.println("hello world 1");
+	}
+}
---
+++ src/helloworldtwo/helloclasstwo.java
@@ -0,0 +1,7 @@
+package helloworldtwo;
+
+public class helloclasstwo {
+	public static void main(String args[]) {
+		System.out.println("hello world 2");
+	}
+}
